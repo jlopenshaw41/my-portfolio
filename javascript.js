0f12712ac0edaf3b4ff39b586792c8394d6bde26
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2020-10-14 17:54:47 +0100

    more adjustments to JS for submit button

--- javascript.js
+++ javascript.js
@@ -2,16 +2,43 @@
 
 let submitButton = document.getElementById('submit-form');
 
-let generatePopUp = () => {
-
-    let name = document.getElementById("name").value;
+const validateForm = () => {
+    
+    let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let message = document.getElementById('message').value;
 
-    alert("Hey " + (name || "there")  + "!\n\nUh oh! I'm afraid your message isn't going to get to me, because I haven't learned how to do that bit yet...\n\nIf you'd really like to get in touch, why not copy your message and pop it in an email to me at jen@jenniferopenshaw.dev. There's even a handy email button at the bottom of the page\n\nI'd love to hear from you!\n\nYour name: " + name + "\nYour email: " + email + "\nYour message: " + message);
+    if (name == "") {
+        alert("Please enter a name");
+    } else if (email == "") {
+        alert("Please enter an email address")
+    } else if (message == "") {
+        alert("Please enter a message")
+    } else {
+        generatePopUp();
+    }
 }
 
-submitButton.addEventListener('click', generatePopUp);
+const generatePopUp = () => {
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    alert(`Hey ${name || "there"}!
+
+    Uh oh! I'm afraid your message isn't going to get to me, because I haven't learned how to do that bit yet...
+
+    If you'd really like to get in touch, why not copy your message from the form and pop it in an email to me at jen@jenniferopenshaw.dev. There's even a handy email button at the bottom of the page.
+
+    I'd love to hear from you!
+
+    Your name: ${name}
+    Your email: ${email}
+    Your message: ${message}`);
+}
+
+submitButton.addEventListener('click', validateForm);
 
 // event listener for 'Clear form' button 
 
