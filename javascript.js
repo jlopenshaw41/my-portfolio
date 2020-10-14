31aa00245cebd57397dd7729adf74f8d4151fb29
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2020-10-14 16:30:40 +0100

    added JS functionality to clear form button

--- javascript.js
+++ javascript.js
@@ -1,11 +1,8 @@
-// event listener for 'Send message' (submit) form button
+// event listener for 'Send message' (submit) button
 
 let submitButton = document.getElementById('submit-form');
 
-
-
-
-let eventHandlerFunction = () => {
+let generatePopUp = () => {
 
     let name = document.getElementById("name").value;
     let email = document.getElementById('email').value;
@@ -14,6 +11,17 @@ let eventHandlerFunction = () => {
     alert("Hey " + (name || "there")  + "!\n\nUh oh! I'm afraid your message isn't going to get to me, because I haven't learned how to do that bit yet...\n\nIf you'd really like to get in touch, why not copy your message and pop it in an email to me at jen@jenniferopenshaw.dev. There's even a handy email button at the bottom of the page\n\nI'd love to hear from you!\n\nYour name: " + name + "\nYour email: " + email + "\nYour message: " + message);
 }
 
-submitButton.addEventListener('click', eventHandlerFunction);
+submitButton.addEventListener('click', generatePopUp);
 
-// event listener for 'Clear form' form button 
+// event listener for 'Clear form' button 
+
+let clearButton = document.getElementById('clear-form');
+
+let clearForm = () => {
+    
+    document.getElementById("name").value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('message').value = "";
+}
+
+clearButton.addEventListener('click', clearForm);
