50349bf5f1aa88a5a9c0f534d839210bc7732cc2
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2020-10-16 16:55:09 +0100

    linked all 3 pages together, added nav, completed styling. Removed copyright text

--- javascript.js
+++ javascript.js
@@ -52,3 +52,15 @@ let clearForm = () => {
 }
 
 clearButton.addEventListener('click', clearForm);
+
+// Nav
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
